94f01185b277d69d221733e3aac29915b8fe6aba
tts2k <tranthaison2000@gmail.com>
2024-03-24 22:09:08 +0700

    feat: numbering for TOC printing

--- lib/scraper.go
+++ lib/scraper.go
@@ -3,6 +3,7 @@ package lib
 import (
 	"fmt"
 	"regexp"
+	"strconv"
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/gocolly/colly"
@@ -89,23 +90,37 @@ func parseMainText(childs *goquery.Selection) []Section {
 	return result
 }
 
-func recurPrintTOC(sections []Section, level int) {
+func recurPrintTOC(sections []Section, prefix string, level int) {
 	var pad string
 	for i := 0; i < level; i++ {
 		pad += "  "
 	}
 
-	for _, section := range sections {
-		fmt.Printf("%s%s\n", pad, section.Title)
+	for index, section := range sections {
+		var sectionNumbering string
+
+		if prefix == "" {
+			sectionNumbering = strconv.Itoa(index+1) + "."
+		} else {
+			sectionNumbering = fmt.Sprintf("%s%d", prefix, index+1)
+		}
+
+		fmt.Printf("%s%s %s\n", pad, sectionNumbering, section.Title)
+
+		// remove the dot on first level
+		if prefix == "" {
+			sectionNumbering = sectionNumbering[:1]
+		}
+
 		if len(section.SubSections) > 0 {
-			recurPrintTOC(section.SubSections, level+1)
+			recurPrintTOC(section.SubSections, sectionNumbering+".", level+1)
 		}
 	}
 }
 
 func (scraper *Scraper) PrintTOC() *Scraper {
-	println(scraper.article.Title)
-	recurPrintTOC(scraper.article.Sections, 0)
+	fmt.Println(scraper.article.Title)
+	recurPrintTOC(scraper.article.Sections, "", 0)
 	return scraper
 }
 
